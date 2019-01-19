0c258114794938bd849f168110eeb9ff45097ec2
venyyan <venyyan@users.noreply.github.com>
2019-01-19 20:31:11 +0200

    declare variables

---
+++ src/SnakeGameNew.java
@@ -0,0 +1,10 @@
+public class SnakeGameNew {
+    static char dir = '•';
+    public static int[] map = new int[] {50, 16};
+    public static int[] tailX, tailY;
+    public static int[] snakeHead = new int[2];
+    public static int[] food = new int[2];
+    public static int snakeLength = 0;
+    public static boolean gameOver = false;
+
+}
